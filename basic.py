ae0f61378da62ec088fed4f62875fef90ba50132
huanglun <492593760@qq.com>
2018-11-12 23:03:21 +0800

    答题

--- basic.py
+++ basic.py
@@ -1,12 +1,15 @@
 # 问题一：字符串排序
 
 s = "hello world"
+new_s = ''.join((lambda x: (x.sort(), x)[1])(list(s)))
 
 # 请编写代码，将 s 以 [a-z] 顺序输出
 
 # 问题二：数值比较
 
-n = [9,15,23,89,33,26,2,76]
+n = [9, 15, 23, 89, 33, 26, 2, 76]
+n_max = max(n)
+n_min = min(n)
 
 # 请编写代码，找出数组中的最大数与最小数
 
@@ -16,3 +19,4 @@ a = "i,am,a,student,in,chengdu"
 
 # 请编写代码，将 “student” 和 “chengdu” 变为可基于参数输入配置的输出
 # 通过参数输入打印出完整的句子
+new_a = 'i,am,a,%s,in,%s' % ('student', 'chengdu')
--- func.py
+++ func.py
@@ -1,9 +1,38 @@
+import re
+
 # 使用 while 循环打印 1 3 5 7 9
+i = 1
+while i < 10:
+    if i % 2 == 1:
+        print(i)
+    i += 1
 
 # 编写一个函数，查找数字 6 是否在列表 l 里，如果在，输出“found”，如果不在，输出“not found”
 
-l = [1,5,7,8,9]
+l = [1, 5, 7, 8, 9]
+
+
+def found(lst):
+    if 6 in lst:
+        print('found')
+    else:
+        print('not found')
+
+
+found(l)
 
 # 将字符串 s 拆分成两个字符串 s1、s2，其中 s1 只包含字母，转换为小写，以 [a-z] 排序，s2 只包含数值，升序排序
 
 s = "aAsnr3id2d4b6gs7DZsf9e1AF"
+
+
+def split_seq(string):
+    s1 = re.findall('[a-z,A-Z]', string)
+    s1.sort()
+    s1 = [s.lower() for s in s1]
+    s2 = re.findall('\d+', string)
+    s2.sort()
+    print(''.join(s1), ''.join(s2))
+
+
+split_seq(s)
